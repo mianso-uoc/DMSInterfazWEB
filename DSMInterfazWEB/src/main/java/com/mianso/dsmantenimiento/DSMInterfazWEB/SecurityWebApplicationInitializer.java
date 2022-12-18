cfe31a17fd2db5f9ce7b511b555a848313853da0
mianso <81926241+mianso-uoc@users.noreply.github.com>
2022-12-18 17:22:10 +0100

    Delete DSMInterfazWEB directory

--- DSMInterfazWEB/src/main/java/com/mianso/dsmantenimiento/DSMInterfazWEB/SecurityWebApplicationInitializer.java
+++
@@ -1,7 +0,0 @@
-package com.mianso.dsmantenimiento.DSMInterfazWEB;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-
-}
--- DSMInterfazWEB/src/main/java/com/mianso/dsmantenimiento/DSMInterfazWEB/enums/IssueStatus.java
+++
@@ -1,7 +0,0 @@
-package com.mianso.dsmantenimiento.DSMInterfazWEB.enums;
-
-public enum IssueStatus {
-	PENDING,
-	SOLVED,
-	CLOSED
-}
